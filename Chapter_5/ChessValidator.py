fc1bda5390564fc0269063d0c29fb3d91b290da6
Paulina Faltyn <paulinafaltyn@Luigi.local>
2024-05-15 13:47:04 +0200

    Update Chess Validator

--- Chapter_5/ChessValidator.py
+++ Chapter_5/ChessValidator.py
@@ -1,42 +1,51 @@
-# Write a function named isValidChessBoard() that takes a dictionary argument 
-# and returns True or False depending on if the board is valid.
-# This function should detect when a bug has resulted in an improper chess board.
-
-# maximum number of pieces for each player
-max_chess_pieces = {
-    'b': {'pawn': 8, 'knight': 2, 'bishop': 2, 'rook': 2, 'queen': 1, 'king': 1},
-    'w': {'pawn': 8, 'knight': 2, 'bishop': 2, 'rook': 2, 'queen': 1, 'king': 1}
-}
-
-# chessboard fields
-chessboard_fields = [
-    '1a', '1b', '1c', '1d', '1e', '1f', '1g', '1h',
-    '2a', '2b', '2c', '2d', '2e', '2f', '2g', '2h',
-    '3a', '3b', '3c', '3d', '3e', '3f', '3g', '3h',
-    '4a', '4b', '4c', '4d', '4e', '4f', '4g', '4h',
-    '5a', '5b', '5c', '5d', '5e', '5f', '5g', '5h',
-    '6a', '6b', '6c', '6d', '6e', '6f', '6g', '6h',
-    '7a', '7b', '7c', '7d', '7e', '7f', '7g', '7h',
-    '8a', '8b', '8c', '8d', '8e', '8f', '8g', '8h'
-]
-
 # Example board:
 players_board = {
     '1a': 'bking',
     '2b': 'wqueen',
 }
 
-def isValidChessBoard(players_board):
-    #  Dictionary to keep track of counts for each player:
-    piece_counts = {
-        'b': {'pawn': 0, 'knight': 0, 'bishop': 0, 'rook': 0, 'queen': 0, 'king': 0},
-        'w': {'pawn': 0, 'knight': 0, 'bishop': 0, 'rook': 0, 'queen': 0, 'king': 0}
-        }
-    # 1) Validate the players board by checking if the input is alphanumeric
+def isValidChessBoard(board):
 
-    # 2) Check if the current number of pieces doesn't exceed the max number of each player by
-    # comparing players_board to the max_chess_pieces dictionary:
+    # make a dictionary to keep count of the players' pieces:
+    piece_count = {}
+    for piece in board.values():
+        if piece in piece_count:
+            piece_count[piece] += 1
+        else:
+            piece_count[piece] = 1
+    
+    # example piece_count after that: {'bking': 1, 'wqueen': 1}
+ 
+    # check if the two kings are on the board:
+    if not ('bking' in piece_count and 'wking' in piece_count):
+        return False
 
-    # 3) Return True if the board is valid, and False if it is invalid
+    # check if there's no more than 16 pieces starting with w and 16 starting with b:
+    for piece in piece_count:
+        if piece[0] == 'w' and piece_count[piece] > 16:
+            return False
+        if piece[0] == 'b' and piece_count[piece] > 16:
+            return False
+        
+    # check for pawns if there are maximum 8 for each color:
+    for piece in piece_count:
+        if piece[1:] == 'pawn' and piece_count[piece] > 8:
+            return False
+        
+   # check if the integer version of first character of each field is between 1 and 8:
+    for field in board:
+        if int(field[0]) < 1 or int(field[0]) > 8:
+            return False
+        
+    # check if the 2nd character of the key is in the range 'abcdefgh':
+    for field in board:
+        if field[1] not in 'abcdefgh':
+            return False
+        
+    return True
+
+test = isValidChessBoard(players_board)
+print(test)
+
 
 
